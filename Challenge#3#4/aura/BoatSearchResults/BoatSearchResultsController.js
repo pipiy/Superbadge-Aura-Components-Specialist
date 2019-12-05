994ffa0fd75769cbf476477c0f6351dbd779443d
razu0031 <abdurrazzak0031@gmail.com>
2019-12-05 19:31:36 +0600

    added challenge#3 and #4 files

---
+++ Challenge#3#4/aura/BoatSearchResults/BoatSearchResultsController.js
@@ -0,0 +1,12 @@
+({
+	initSearch : function(component, event, helper) {
+		helper.onSearch(component);
+	},
+    
+    doSearch : function(component, event, helper) {
+        var params = event.getParam('arguments');
+        component.set("v.boatTypeId", params.boatTypeId);
+		helper.onSearch(component);
+	},
+    
+})
